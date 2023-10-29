8fff5dcd3a664541aa5e9445df3070e9c9d19deb
Ivan <ivan_ivanov_3000@abv.bg>
2023-10-30 01:45:51 +0200

    Add the logic for years.cpp

--- week-one/homwork/years.cpp
+++ week-one/homwork/years.cpp
@@ -2,10 +2,21 @@
 
 int main()
 {
-    //we convert the years into days
-    std::cout << "Hello world " << "\n";
-    std::cout << "Hello world ";
+    int year, month, day, hours, mins, secs;
+    std::cin >> year;
+    month = year * 12;
+    day = year * 365;
+    hours = day * 24;
+    mins = hours * 60;
+    secs = mins * 60;
 
+    std::cout 
+    << "Months old: " << month << '\n'
+    << "Days old: " << day << '\n'
+    << "Hours old: " << hours << '\n'
+    << "Minutes old: " << mins << '\n'
+    << "Secs old: " << secs 
+    << std::endl;
 
     return 0;
 }
