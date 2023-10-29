5a6a0bc8c219482b17bd9aa9ae1f083110b9bcb5
Ivan <ivan_ivanov_3000@abv.bg>
2023-10-30 01:55:04 +0200

    Update the cordinates.cpp so that it handels all cases

--- week-one/homework/cordinates.cpp
+++ week-one/homework/cordinates.cpp
@@ -1,28 +1,49 @@
 #include <iostream>
 
-int main(){
+int main()
+{
 
-    int x,y;
-
-    if(x > 0){
-        if(y> 0){
+    int x, y;
+    std::cin >> x >> y;
+    if (x > 0)
+    {
+        if (y > 0)
+        {
             std::cout << "1st";
-            return;
+            return 0;
         }
-        else if(y<0){
+        else if (y < 0)
+        {
             std::cout << "4th";
-            return;
-
+            return 0;
         }
-    }else if(x < 0){
-        if(y> 0){
+        else
+        {
+            std::cout << "Can not be determined";
+            return 0;
+        }
+    }
+    else if (x < 0)
+    {
+        if (y > 0)
+        {
             std::cout << "2nd";
-            return;
+            return 0;
         }
-        else if(y<0){
+        else if (y < 0)
+        {
             std::cout << "3rd";
-            return;
-
+            return 0;
         }
+         else
+        {
+            std::cout << "Can not be determined";
+            return 0;
+        }
+    }
+    else
+    {
+        std::cout << "Can not be determined";
+        return 0;
     }
 }
